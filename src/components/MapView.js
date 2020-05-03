cd4f7f9c4f3693746ca94873bec8e36bbbc44662
Shiwei Zhou <252485484@qq.com>
2020-05-02 18:06:18 -0700

    multiple marker

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -10,13 +10,40 @@ class MapView extends Component {
           lat: this.props.value.lat,
           lng: this.props.value.lng,
         },
+        sorrounding: [],
         zoom: 11,
       },
+      selectedMarker: { index: -1, lat: null, lng: null },
     };
+    this.renderMarkers = this.renderMarkers.bind(this);
   }
 
   componentDidMount() {
     //use this to fetch sorrounding points
+    let dummy_sorrounding = [
+      { index: 122, lat: 32.7, lng: -117.16, title: "dummy 1" },
+      { index: 124, lat: 32.7, lng: -117.26, title: "dummy 2" },
+      { index: 128, lat: 32.74, lng: -117.16, title: "dummy 3" },
+      { index: 121, lat: 32.74, lng: -117.26, title: "dummy 4" },
+    ];
+    this.setState((state) => {
+      state.mapInfo.sorrounding = dummy_sorrounding;
+    });
+  }
+  renderMarkers(map, maps) {
+    let marker_list = [];
+    //instantiate marker obj for the map
+    for (let i = 0; i < this.state.mapInfo.sorrounding.length; i++) {
+      let point = this.state.mapInfo.sorrounding[i];
+      let marker = new maps.Marker({
+        position: { lat: point.lat, lng: point.lng },
+        map,
+        title: point.title,
+      });
+      marker_list.push(marker);
+      console.log(point);
+    }
+    return marker_list;
   }
 
   render() {
@@ -36,6 +63,8 @@ class MapView extends Component {
           bootstrapURLKeys={{ key: "AIzaSyAu9NbHrFz1kZp8s_nxGqetjLuwswlObwc" }}
           defaultCenter={this.state.mapInfo.center}
           defaultZoom={this.state.mapInfo.zoom}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => this.renderMarkers(map, maps)}
         ></GoogleMapReact>
       </div>
     );
